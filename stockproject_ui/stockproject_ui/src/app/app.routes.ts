227b103c17db9555e50f8becaea1333316d6ebdb
OussamaELGUERRAOUI <124736443+OussamaELGUERRAOUI@users.noreply.github.com>
2025-07-07 02:27:48 +0200

    formulaire

--- stockproject_ui/stockproject_ui/src/app/app.routes.ts
+++ stockproject_ui/stockproject_ui/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 // import { AccueilComponent } from './accueil/accueil.component';
 import { GestionUtilisateursComponent } from './gestion-utilisateurs/gestion-utilisateurs';
 import { Accueil} from './accueil/accueil';
+import {Formulaire} from './formulaire/formulaire'
 export const routes: Routes = [
+    { path: '', component: Accueil },
   { path: 'gestion_utilisateur', component: GestionUtilisateursComponent },
   { path: 'accueil', component: Accueil },
-  { path: '', component: Accueil },
+  { path: 'formualire', component: Formulaire },
 ];
 
 @NgModule({
